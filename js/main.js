89e0143478b6b7de555b763a83e8a42d7cfd5b3a
Tania <taniie_bcn@hotmail.es>
2023-09-08 11:17:30 +0200

    add conditionals

--- js/main.js
+++ js/main.js
@@ -1,11 +1,29 @@
 'use strict'
 
 const list = document.querySelector(".js-list");
+
+const kittenRace1 = 'Siamés';
 const kittenImage1 = 'https://dev.adalab.es/gato-siames.webp';
 const kittenName1 = 'Anastacio';
 const kittenDesc1 =
   ' Porte elegante, su patrón de color tan característico y sus ojos de un azul intenso, pero su historia se remonta a Asía al menos hace 500 años, donde tuvo su origen muy posiblemente.';
-const kittenRace1 = 'Siamés';
+
+const kittenRace2 = 'Sphynx';
+const kittenImage2 = 'https://dev.adalab.es/sphynx-gato.webp';
+const kittenName2 = 'Fiona';
+const kittenDesc2 =
+   `Produce fascinación y curiosidad. Exótico, raro, bello, extraño…
+  hasta con pinta de alienígena han llegado a definir a esta raza
+  gatuna que se caracteriza por la «ausencia» de pelo.`;
+
+const kittenRace3 = 'Maine Coon';
+const kittenImage3 = 'https://dev.adalab.es/maine-coon-cat.webp';
+const kittenName3 = 'Cielo';
+const kittenDesc3 =
+  `Tienen la cabeza cuadrada y los ojos simétricos, por lo que su
+  bella mirada se ha convertido en una de sus señas de identidad.
+  Sus ojos son grandes y las orejas resultan largas y en punta.`;
+
 
 const kittenOne = `<li class="card">
 <article>
@@ -14,7 +32,7 @@ const kittenOne = `<li class="card">
     src="https://dev.adalab.es/gato-siames.webp"
     alt="siames-cat"
   />
-  <h3 class="card_title">`${kittenName1}`</h3>
+  <h3 class="card_title">${kittenName1.toUpperCase()}</h3>
   <h4 class="card_race">Siamés</h4>
   <p class="card_description">
     Porte elegante, su patrón de color tan característico y sus ojos
@@ -29,7 +47,7 @@ const kittenTwo =`<li class="card">
   src="https://dev.adalab.es/sphynx-gato.webp"
   alt="sphynx-cat"
 />
-<h3 class="card_title">Fiona</h3>
+<h3 class="card_title">${kittenName2.toUpperCase()}</h3>
 <h4 class="card_race">Sphynx</h4>
 <p class="card_description">
   Produce fascinación y curiosidad. Exótico, raro, bello, extraño…
@@ -43,7 +61,7 @@ const kittenThree = `<li class="card">
   src="https://dev.adalab.es/maine-coon-cat.webp"
   alt="maine-coon-cat"
 />
-<h3 class="card_title">Cielo</h3>
+<h3 class="card_title">${kittenName3.toUpperCase()}</h3>
 <h4 class="card_race">Maine Coon</h4>
 <p class="card_description">
   Tienen la cabeza cuadrada y los ojos simétricos, por lo que su
@@ -56,4 +74,17 @@ list.innerHTML += kittenTwo;
 list.innerHTML += kittenThree;
 
 
+const input_search_desc = document.querySelector('.js_in_search_desc');
+const descrSearchText = input_search_desc.value;
 
+
+if( kittenDesc1.includes(elegante) ) {  
+  list.innerHTML = 'kittenOne';
+  }  
+  if( kittenDesc2.includes(Exótico) ) {
+    list.innerHTML = 'kittenTwo';
+  }
+  if ( kittenDesc3.includes(simétricos) ) {
+    list.innerHTML = 'kittenThree';
+  }
+    
